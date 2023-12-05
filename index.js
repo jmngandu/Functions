d1c20cf4f76432cd140930ca6b0bfbc1a147e2c1
jmngandu <jamesmngandu@gmail.com>
2023-12-05 07:39:08 +0300

    Commit

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const name = "james";
 const age = 20;
 
-if (name){
-    console.log(`Your name is ${name}`);
+function firstName(){
+
+    if (age < 21 && age > 19){
+        return `Your name is ${name}`
+    }else
+    {
+
+    return false
+}
 }
