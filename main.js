c4b03afe51f4fe8ae810988030b4c803e1b4a5e5
jmngandu <jamesmngandu@gmail.com>
2023-12-26 22:06:30 +0300

    commit

--- main.js
+++ main.js
@@ -2,6 +2,7 @@
 Tag = ["Kenya", "Uganda", "Tanzania", "Rwanda"];
 Tag.pop();//pop is used to remove the last word
 Tag.shift()//shift is used remove the first word
+Tag.push("Burundi")//push can add a word at the last
 document.querySelector(".clicker").innerText = Tag.toString()//to print the string
 document.querySelector(".clicke").innerText = Tag.join(" - ")//Join is used to separate array elements 
 
