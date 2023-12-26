bae7a591db399f870e5b66795d3de8dc2d1cb801
jmngandu <jamesmngandu@gmail.com>
2023-12-26 22:13:12 +0300

    commit

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 
-Tag = ["Kenya", "Uganda", "Tanzania", "Rwanda"];
+countries = ["Kenya", "Uganda", "Tanzania", "Rwanda"];
 Tag.pop();//pop is used to remove the last word
 Tag.shift()//shift is used remove the first word
 Tag.push("Burundi")//push adds a word to the array
 Tag.unshift("Somalia")//unshift adds a word to the beginning of an array
-document.querySelector(".clicker").innerText = Tag.toString()//to print the string
+delete 
+document.querySelector(".clicker").innerText = countries.toString()//to print the string
 document.querySelector(".clicke").innerText = Tag.join(" - ")//Join is used to separate array elements 
 
 
