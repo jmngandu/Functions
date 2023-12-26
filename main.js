6120e5ea46af190c71047cab930d98d6fd2baadb
jmngandu <jamesmngandu@gmail.com>
2023-12-26 21:54:44 +0300

    commit

---
+++ main.js
@@ -0,0 +1,4 @@
+
+Tag = ["Kenya", "Uganda", "Tanzania", "Rwanda"]
+document.querySelector(".clicker").innerText = Tag.toString()//to print the string
+document.querySelector(".clicke").innerText = Tag.join(" - ")//Join is used to separate array elements 
