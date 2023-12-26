3ef5a26a41cc1bcee7e859e91d98654a303351f0
jmngandu <jamesmngandu@gmail.com>
2023-12-26 22:14:00 +0300

    commit

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 
 countries = ["Kenya", "Uganda", "Tanzania", "Rwanda"];
-Tag.pop();//pop is used to remove the last word
-Tag.shift()//shift is used remove the first word
+countries.pop();//pop is used to remove the last word
+countries.shift()//shift is used remove the first word
 Tag.push("Burundi")//push adds a word to the array
 Tag.unshift("Somalia")//unshift adds a word to the beginning of an array
 delete 
