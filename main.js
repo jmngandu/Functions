f12a02485cb04922c4ae59d492ff9083769c6dd6
jmngandu <jamesmngandu@gmail.com>
2023-12-26 22:14:19 +0300

    commit

--- main.js
+++ main.js
@@ -2,8 +2,8 @@
 countries = ["Kenya", "Uganda", "Tanzania", "Rwanda"];
 countries.pop();//pop is used to remove the last word
 countries.shift()//shift is used remove the first word
-Tag.push("Burundi")//push adds a word to the array
-Tag.unshift("Somalia")//unshift adds a word to the beginning of an array
+countries.push("Burundi")//push adds a word to the array
+countries.unshift("Somalia")//unshift adds a word to the beginning of an array
 delete 
 document.querySelector(".clicker").innerText = countries.toString()//to print the string
 document.querySelector(".clicke").innerText = countries.join(" - ")//Join is used to separate array elements 
