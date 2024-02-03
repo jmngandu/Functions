2f80132d6c30f39fb84cea676d4507b3da3c4fd5
jmngandu <jamesmngandu@gmail.com>
2024-02-03 23:31:17 +0300

    commi

--- maps/tempCodeRunnerFile.js
+++ maps/tempCodeRunnerFile.js
@@ -2,11 +2,17 @@
 Challenge 1:
 Given an array of numbers, return an array of each number, squared
 */
-const nums = [1, 2, 3, 4, 5];
+const nums = [
+    1,
+    2,
+    3,
+    4,
+    5
+];
 // -->       [1, 4, 9, 16, 25]
 // Your code here
 const squares = nums.map(function (num) {
-	return num * num;
+    return num * num;
 });
 
 console.log(squares);
@@ -21,7 +27,7 @@ const names = ['alice', 'bob', 'charlie', 'danielle'];
 // -->        ["Alice", "Bob", "Charlie", "Danielle"]
 // Your code here
 const capitalized = names.map((name) => {
-	return name[0].toUpperCase() + name.slice(1);
+    return name[0].toUpperCase() + name.slice(1);
 });
 
 console.log(capitalized);
