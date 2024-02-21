79ff1875f6b12a2498f5c309f7152e6c3b0e1e5e
jmngandu <jamesmngandu@gmail.com>
2024-02-21 21:33:29 +0300

    commit

--- ES6+/padStart-padEnd.js
+++ ES6+/padStart-padEnd.js
@@ -3,5 +3,4 @@ let names = 'james mukuvi';
 // padstart() adds values to the beginning of the variable set
 // padEnd() adds values to the ending of the variable set
 console.log(names.padStart(15, '03'))
-
 console.log(name.padEnd(20, 'Ngandu'));
