ae082fffceea9b40391c6c05aa846a772cab0d5e
jmngandu <jamesmngandu@gmail.com>
2024-07-25 20:52:39 +0300

    function

--- DOM/General-DOM/main.js
+++ DOM/General-DOM/main.js
@@ -7,3 +7,9 @@ console.log(document.URL);
 const h1 = document.querySelector("h1");
 h1.classList.add("styles");
 h1.classList.remove("styles");
+
+//toggle - if you dont have class it adds and if you have class it removes
+h1.classList.toggle("hello");
+//attributes
+const a = document.querySelector("a");
+console.log(a.href);
