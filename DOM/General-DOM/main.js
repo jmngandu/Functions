675d3ddabebecc6b58ad48dd9784b84b1a5310bd
jmngandu <jamesmngandu@gmail.com>
2024-07-25 20:04:48 +0300

    functions

--- DOM/General-DOM/main.js
+++ DOM/General-DOM/main.js
@@ -2,3 +2,8 @@ console.log(document.title);
 console.log(document.head);
 console.log(document.body);
 console.log(document.URL);
+//textContent, innerText, innerHTML
+//class list
+const h1 = document.querySelector("h1");
+h1.classList.add("styles");
+h1.classList.remove("styles");
