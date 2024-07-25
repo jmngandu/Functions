d5cbda0b4125e3deae5189a2d5c7ccc66046596f
jmngandu <jamesmngandu@gmail.com>
2024-07-25 22:18:53 +0300

    function

--- DOM/General-DOM/main.js
+++ DOM/General-DOM/main.js
@@ -13,3 +13,10 @@ h1.classList.toggle("hello");
 //attributes
 const a = document.querySelector("a");
 console.log(a.href);
+h1.style.color = "teal";
+h1.style.backgroundColor = "#fff";
+
+let telMe = document.querySelector(".telMe");
+let telI = document.createElement("h1");
+telI.innerText = "hello";
+telMe.appendChild(telI);
