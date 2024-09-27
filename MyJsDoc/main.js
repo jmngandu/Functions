2b74ecfe44e4845f030c5f2f86d44d9819e07e3a
jmngandu <jamesmngandu@gmail.com>
2024-09-28 02:19:42 +0300

    functions

--- MyJsDoc/main.js
+++ MyJsDoc/main.js
@@ -317,11 +317,7 @@ function someone(first, last, ...hobbies) {
 const family = ["father", "mother", "children"];
 
 const [x, y, z] = family;
-const myno = [2, 3, 4, 5, 6];
-const totalno = myno.map((theno) => {
-  return theno * 2;
-});
-console.log(totalno);
+
 // console.log(x);
 
 function f() {
