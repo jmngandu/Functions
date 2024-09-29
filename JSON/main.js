07fb9575493fc99232428016d78db770b27291dd
jmngandu <jamesmngandu@gmail.com>
2024-09-29 23:55:40 +0300

    fetch data

--- JSON/main.js
+++ JSON/main.js
@@ -2,17 +2,24 @@ const btn = document.querySelector(".btn");
 
 btn.addEventListener("click", makeRequest);
 function makeRequest() {
-  fetch("https://jsonplaceholder.typicode.com/posts/1")
+  fetch("https://jsonplaceholder.typicode.com/posts")
     .then((res) => {
       if (!res.ok) new Error(res.statusText);
       return res.json();
     })
     .then((data) => {
-      document.querySelector(".id").innerHTML = data.id;
-      document.querySelector(".title").innerHTML = data.title;
-      document.querySelector(".body").innerHTML = data.body;
+      //   document.querySelector(".id").innerHTML = data.id;
+      //   document.querySelector(".title").innerHTML = data.title;
+      //   document.querySelector(".body").innerHTML = data.body;
+      let output = document.querySelector(".all-posts");
+      data.forEach((element) => {
+        output.innerHTML += `
+        <br>
+        <div>(ID): ${element.id}</div>
+        <div>(TITLE): ${element.title}</div>
+        <div>(BODY): ${element.body}</div>
+        `;
+      });
     })
-    .catch((error) => {
-      console.log(error);
-    });
+    .catch((error) => console.log(error));
 }
